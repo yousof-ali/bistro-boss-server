e4b5a77988f35d1985b6ecc36461fbce175be5cf
yousof-ali <yousofali293@gmail.com>
2025-04-06 20:22:25 +0600

    payment method

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
@@ -66,7 +67,19 @@ async function run() {
        }
 
 
-
+    //    payment method 
+    app.post('/create-payment-intent',async(req,res) => {
+        const{price} = req.body;
+        const amount = parseInt(price * 100);
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount:amount,
+            currency:'usd',
+            payment_method_types:['card']
+        });
+        res.send({
+            clientSecret:paymentIntent.client_secret
+        })
+    })
 
 
         app.post('/jwt',async(req,res) => {
@@ -128,6 +141,47 @@ async function run() {
             const result = await menuCollections.find().toArray();
             res.send(result);
         });
+        
+        app.post('/menu',verifyToken,verifyAdmin,async(req,res) => {
+            const data = req.body;
+            const result = await menuCollections.insertOne(data);
+            res.send(result);
+        })
+
+        app.delete('/menu-delete/:id',verifyToken,verifyAdmin,async(req,res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = {_id : new ObjectId(id)};
+            const result = await menuCollections.deleteOne(query);
+            res.send(result);
+        })
+
+        app.get('/single-menu/:id',async(req,res) => {
+            const id = req.params.id 
+            console.log(id)
+            const   query = {_id : new ObjectId(id)}
+            const result = await menuCollections.findOne(query);
+            console.log(result)
+            res.send(result);
+        })
+
+        app.patch('/menu-update/:id',async(req,res) => {
+            const newData = req.body;
+            const id = req.params.id
+            const query = {_id:new ObjectId(id)}
+            const updateDoc = {
+                $set:{
+                    name:newData.name,
+                    category:newData.category,
+                    price:newData.price,
+                    recipe:newData.recipe
+
+                }
+            }
+            const result = await menuCollections.updateOne(query,updateDoc)
+            res.send(result);
+        })
+
         app.post('/cart',async(req,res) => {
             const item = req.body;
             const result = await cartCollections.insertOne(item);
